8b9a6befdc2ffc6e19ce32cb26d628d21940c717
Vlad <lll-ooo-2003@mail.ru>
2020-05-09 22:37:52 +0400

    переработанны класс фильтра

--- Classes/Classificate.py
+++ Classes/Classificate.py
@@ -10,48 +10,103 @@ class ColorClassif(IClassesificationType):
     __high = None
 
     def img_approximation(self, img):
-        img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-        img = cv2.medianBlur(img, 5)
+        img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
         low_color = np.array(self.__low, np.uint8)
         high_color = np.array(self.__high, np.uint8)
-        img_filter = cv2.inRange(img, low_color, high_color)
-        return img_filter
+        img_filter = cv2.inRange(img_hsv, low_color, high_color)
+        img = cv2.bitwise_and(img, img, mask=img_filter)
+        cv2.imwrite("img/mask.jpg", img)
+        img = cv2.medianBlur(img, 5)
+        img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        cv2.imwrite("img/Blur.jpg", img_gray)
+        img_filter = cv2.Canny(img_gray, 10, 250)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (5, 5))
+        closed = cv2.morphologyEx(img_filter, cv2.MORPH_CLOSE, kernel)
+        cv2.imwrite("img/close.jpg", closed)
+        return closed
 
     def __init__(self, low, high):
         self.__low = low
         self.__high = high
 
     def cycle(self, img):
-        contours, _ = cv2.findContours(self.img_approximation(img), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contours = cv2.findContours(self.img_approximation(img), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
         cnt_list = []
+        max_area = 0
         for cnt in contours:
-            approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
-            if len(cnt) > 100 and len(approx) > 10:
+            approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
+            line_len = len(cnt)
+            if line_len > 100 and len(approx) >= 8:
                 ellipse = cv2.fitEllipse(cnt)
-                cnt_list.append([int(ellipse[0][0]), int(ellipse[0][1]), 'cycle'])
-        return cnt_list
+                max_area = line_len if line_len > max_area else max_area
+                cnt_list.append((int(ellipse[0][0]), int(ellipse[0][1]), line_len, len(approx)))
+        squ = list(filter(lambda x: x[3] >= 8 and x[2] == max_area, cnt_list))
+        return [[squ[0][0], squ[0][1], 'cycle']]
 
     def square(self, img):
-        contours, _ = cv2.findContours(self.img_approximation(img), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        cnt_list = []
+        '''contours = cv2.findContours(self.img_approximation(img), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+        sqr_list = []
+        min_area = 10000000
         for cnt in contours:
             x, y, w, h = cv2.boundingRect(cnt)
+            approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
+            area = w * h / 100
+            cv2.drawContours(img, [approx], -1, (255, 0, 0), 4)
+            cv2.imwrite("img/test.jpg", img)
+            print(area, len(approx))
+            if len(approx) == 4 and area > 100:
+                min_area = area if min_area > area else min_area
+                sqr_list.append((len(approx), w * h / 100, int(x + w / 2), int(y + h / 2)))
+        squ = list(filter(lambda x: x[0] == 4 and x[1] == min_area, sqr_list))
+        return [[squ[0][2], squ[0][3], 'square']]'''
+        img_new = self.img_approximation(img)
+        linse = cv2.HoughLinesP(img_new, 1, np.pi / 180, 50, minLineLength=30, maxLineGap=10)
+        contours = cv2.findContours(img_new, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
+        cnt_list = []
+        for cnt in contours:
             approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
-            if len(approx) < 7 and w * h / 100 > 290:
+            x, y, w, h = cv2.boundingRect(cnt)
+            #  print(len(approx), w * h / 100)
+            if len(approx) == 4 and 170 >= w * h / 100 > 0.5:
                 cnt_list.append([int(x + w / 2), int(y + h / 2), 'square'])
-        return cnt_list
+        cnt_list.sort(key=lambda i: i[0] and i[1], reverse=False)
+        linse_in_squ = []
+        center_list = []
+        for i in cnt_list:
+            for j in linse:
+                x_mid = int((j[0][0] + j[0][2]) / 2)
+                y_mid = int((j[0][1] + j[0][3]) / 2)
+                if fabs(i[1] - y_mid) < 45 and fabs(i[0] - x_mid) < 45:
+                    linse_in_squ.append(j)
+            if len(linse_in_squ) == 0:
+                center_list.append(i)
+                break
+            else:
+                linse_in_squ.clear()
+        return center_list
 
     def square_with_a_hole(self, img):
-        contours, _ = cv2.findContours(self.img_approximation(img), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        img_new = self.img_approximation(img)
+        linse = cv2.HoughLinesP(img_new, 1, np.pi / 180, 50, minLineLength=30, maxLineGap=10)
+        contours = cv2.findContours(img_new, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[0]
         cnt_list = []
         for cnt in contours:
-            approx = cv2.approxPolyDP(cnt, 0.01 * cv2.arcLength(cnt, True), True)
+            approx = cv2.approxPolyDP(cnt, 0.001 * cv2.arcLength(cnt, True), True)
             x, y, w, h = cv2.boundingRect(cnt)
-            if len(approx) < 9 and 290 > w * h / 100 > 0.5:
+            #  print(len(approx), w * h / 100)
+            if len(approx) > 4 and 170 >= w * h / 100 > 0.5:
                 cnt_list.append([int(x + w / 2), int(y + h / 2), 'square_with_a_hole'])
-        cnt_list.sort(key=lambda i: i[0], reverse=True)
+        cnt_list.sort(key=lambda i: i[0] and i[1], reverse=False)
+        linse_in_squ = []
         center_list = []
-        for i in range(0, len(cnt_list), 2):
-            if fabs(cnt_list[i][0] - cnt_list[i][0]) < 20:
-                center_list.append(cnt_list[i])
+        for i in cnt_list:
+            for j in linse:
+                x_mid = int((j[0][0] + j[0][2]) / 2)
+                y_mid = int((j[0][1] + j[0][3]) / 2)
+                if fabs(i[1] - y_mid) < 45 and fabs(i[0] - x_mid) < 45:
+                    linse_in_squ.append(j)
+            if 5 > len(linse_in_squ) > 0:
+                center_list.append(i)
+                break
         return center_list
+
--- MainCl.py
+++ MainCl.py
@@ -1,15 +1,58 @@
 import cv2
 from Classes import Classificate
+import numpy as np
 
-img_uri = "tuA-YNuPOsE.jpg"
+img_uri = "RBkvD8nyo-4.jpg"
+
+
+def nothing(x):
+    pass
+
 
 img = cv2.imread(img_uri)
-filter_red = Classificate.ColorClassif((0, 50, 150), (10, 255, 255))
-filter_blue = Classificate.ColorClassif((94, 80, 2), (126, 255, 255))
-filter_green = Classificate.ColorClassif((35, 150, 50), (75, 255, 255))
-filter_list = [filter_red, filter_blue, filter_green]
+filter_red = Classificate.ColorClassif((164, 93, 121), (255, 255, 255))
+filter_blue = Classificate.ColorClassif((86, 132, 25), (162, 255, 255))
+filter_green = Classificate.ColorClassif((70, 93, 42), (95, 255, 255))
+filter_list = [filter_green, filter_red, filter_blue]
 
 list_center = []
+
+print(filter_blue.square_with_a_hole(img))
+print(filter_blue.cycle(img))
+print(filter_blue.square(img))
+
+def test():
+    cv2.namedWindow("Tracking")
+    cv2.createTrackbar("LowH", "Tracking", 0, 255, nothing)
+    cv2.createTrackbar("LowS", "Tracking", 0, 255, nothing)
+    cv2.createTrackbar("LowV", "Tracking", 0, 255, nothing)
+    cv2.createTrackbar("UnH", "Tracking", 255, 255, nothing)
+    cv2.createTrackbar("UnS", "Tracking", 255, 255, nothing)
+    cv2.createTrackbar("UnV", "Tracking", 255, 255, nothing)
+    while True:
+        frame = cv2.imread(img_uri)
+        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        l_h = cv2.getTrackbarPos("LowH", "Tracking")
+        l_s = cv2.getTrackbarPos("LowS", "Tracking")
+        l_v = cv2.getTrackbarPos("LowV", "Tracking")
+
+        h_h = cv2.getTrackbarPos("UnH", "Tracking")
+        h_s = cv2.getTrackbarPos("UnS", "Tracking")
+        h_v = cv2.getTrackbarPos("UnV", "Tracking")
+
+        l_d = np.array([l_h, l_s, l_v])
+        h_d = np.array([h_h, h_s, h_v])
+        mask = cv2.inRange(hsv, l_d, h_d)
+        mask = cv2.medianBlur(mask, 5)
+        res = cv2.bitwise_and(frame, frame, mask=mask)
+
+        cv2.imshow("Frame", frame)
+        cv2.imshow("Mask", mask)
+        cv2.imshow("REs", res)
+
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
 
 
 def main_classificate(img):
@@ -23,5 +66,8 @@ def main_classificate(img):
             img = cv2.circle(img, (j[0], j[1]), 3, (255, 255, 255), 2)
     cv2.imwrite("now.jpg", img)
 
-
 main_classificate(img)
+#  test()
+
+img = cv2.Canny(img, 100, 200)
+cv2.imwrite("canny.jpg", img)
